f03d1a4bb85925252d171d988ac05cc5e2220a45
Michael Adelmann <noreply@github.com>
2025-01-04 12:56:18 +0100

    implemented upload script

---
+++ upload/index.php
@@ -0,0 +1,42 @@
+<?php
+
+//print_r( $_SERVER );
+//print_r( $_FILES );
+//print_r( $_REQUEST );
+//print_r( $_POST );
+
+$allowedTypes = [ "application/json", "application/tar+gzip" ];
+
+$receivedContentType = $_FILES[ "file" ][ "type" ];
+if ( !$receivedContentType ) {
+	$receivedContentType = $_SERVER[ "CONTENT_TYPE" ];
+}
+
+if ( !in_array( $receivedContentType, $allowedTypes ) ) {
+	echo json_encode(['success' => false, 'message' => 'Unsupported file type.']);
+	exit;
+}
+
+if ( ( $_SERVER['REQUEST_METHOD'] == 'POST' || $_SERVER['REQUEST_METHOD'] == 'PUT' ) && isset($_FILES['file']) ) {
+    //$uploadDir = "";
+    //$uploadFile = $uploadDir . basename($_FILES['file']['name']);
+
+    // Create the uploads directory if it doesn't exist
+    //if (!is_dir($uploadDir)) {
+    //    mkdir($uploadDir, 0777, true);
+    //}
+
+    $uploadFile = basename($_FILES['file']['name']);
+
+    if ( move_uploaded_file( $_FILES['file']['tmp_name'], $uploadFile ) ) {
+        //echo "File is valid, and was successfully uploaded.\n";
+    }
+    else {
+        echo "Possible file upload attack!\n";
+    }
+}
+else {
+    echo "No file uploaded.\n";
+}
+?>
+
