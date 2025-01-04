436a7e4a1c5d24240246bfdfc789c479ecf39989
Michael Adelmann <noreply@github.com>
2025-01-04 14:57:50 +0100

    refactored repository index generation to sort modules by name and module versions by inverse name

--- generateIndex.py
+++ generateIndex.py
@@ -3,39 +3,47 @@
 import os
 import json
 
-def generate_index(repository_path):
-    index = {"modules": []}
+def generate_index( repository_path ):
+    index = { "modules": [] }
 
-    for module_name in os.listdir(repository_path):
-        module_path = os.path.join(repository_path, module_name)
+    for module_name in sorted( os.listdir( repository_path ) ):
+        module_path = os.path.join( repository_path, module_name )
 
-        if os.path.isdir(module_path):
-            for version in os.listdir(module_path):
-                version_path = os.path.join(module_path, version)
+        if os.path.isdir( module_path ):
+            versions = os.listdir( module_path )
 
-                if os.path.isdir(version_path):
-                    config_path = os.path.join(version_path, "module.json")
+            # Filter to include only folders
+            #versions = [ item for item in versions if os.path.isdir( os.path.join( module_path, item ) ) ]
 
-                    if os.path.exists(config_path):
-                        with open(config_path) as f:
+            # Sort the folders alphabetically, reversed if specified
+            versions.sort( reverse=True )
+
+            for version in versions:
+                version_path = os.path.join( module_path, version )
+
+                if os.path.isdir( version_path ):
+                    module_path = os.path.join(version_path, "module.json")
+
+                    if os.path.exists( module_path ):
+                        with open( module_path ) as f:
                             try:
-                                config = json.load(f)
+                                module = json.load( f )
                             except:
-                                print(f"Error: {config_path}")
+                                print( f"Error: {module_path}" )
                                 continue
 
-                        index["modules"].append({
-                            "name": config["name_short"],
-                            "name_full": config["name"],
-                            "version": config["version"],
+                        index["modules"].append( {
+                            "name": module["name_short"],
+                            "name_full": module["name"],
+                            "version": module["version"],
                             "path": f"/{module_name}/{version}/module.json"
-                        })
+                        } )
     return index
 
 # Generate and save the index.json
 repository_path = "modules"
-index = generate_index(repository_path)
+index = generate_index( repository_path )
 
-with open(os.path.join(repository_path, "index.json"), "w") as f:
-    json.dump(index, f, indent=2)
+with open( os.path.join( repository_path, "index.json" ), "w" ) as f:
+    json.dump( index, f, indent=2 )
 
